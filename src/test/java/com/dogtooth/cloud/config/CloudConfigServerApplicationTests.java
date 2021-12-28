eb20e6213659f8d53a4bba3005791bc7aa184fb7
Eva Du lieu <eva.dulieu@gmail.com>
2021-12-28 19:27:20 +0000

    Initial version

---
+++ src/test/java/com/dogtooth/cloud/config/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogtooth.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
